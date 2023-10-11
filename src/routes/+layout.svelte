ca9277c423d7ed13a18bd18a35f2c24989a42d98
Jonah Bedouch <jonah@bedouch.net>
2023-10-11 15:01:42 -0700

    Add loading animation and upadte routing to allow for multi-page member portal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,14 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 	import { user, userData } from '$lib/authStore';
-	import { get } from 'svelte/store';
+	import { get, writable } from 'svelte/store';
+	import { page } from '$app/stores';
 	import type { User } from 'firebase/auth';
 	import { goto } from '$app/navigation';
 	import { getUserData, isCandidateData } from '$lib/utils';
+	import Card from '$lib/components/Card.svelte';
+
+	const loaded = writable(false);
 
 	onMount(async () => {
 		await user.known;
@@ -14,11 +18,14 @@
 		const userState: User | null = get(user);
 		if (userState == null) {
 			goto('/login');
+			$loaded = true;
+			return;
 		}
 
 		const data = await getUserData($user);
 		if (data == null || data == undefined) {
 			goto('/invalidAccount');
+			$loaded = true;
 			return;
 		}
 
@@ -27,11 +34,18 @@
 			if (isCandidateData(data)) {
 				goto('/candportal');
 			} else {
+				if (!$page.url.pathname.startsWith('/memberportal')) {
+					goto('/memberportal');
+				}
 				goto('/invalidAccount');
 			}
+			$loaded = true;
 			return;
 		}
-		goto('/memberportal');
+		if (!$page.url.pathname.startsWith('/memberportal')) {
+			goto('/memberportal');
+		}
+		$loaded = true;
 	});
 </script>
 
@@ -39,6 +53,72 @@
 	<Header />
 
 	<main class="flex min-h-screen container mx-auto px-4 overflow-y-scroll">
-		<slot />
+		{#if loaded}
+			<slot />
+		{:else}
+			<div class="flex flex-col w-full">
+				<Card className="h-min">
+					<div class="animate-pulse">
+						<div class="flex-1 space-y-6 py-1">
+							<div class="h-4 bg-gray-700 rounded-xl" />
+							<div class="space-y-3">
+								<div class="grid grid-cols-3 gap-4">
+									<div class="h-4 bg-gray-700 rounded-xl col-span-2" />
+									<div class="h-4 bg-gray-700 rounded-xl col-span-1" />
+								</div>
+								<div class="h-4 bg-gray-700 rounded-xl" />
+							</div>
+						</div>
+					</div>
+				</Card>
+
+				<Card className="h-min">
+					<div class="animate-pulse">
+						<div class="flex-1 space-y-6 py-1">
+							<div class="h-4 bg-gray-700 rounded-xl" />
+							<div class="space-y-3">
+								<div class="h-4 bg-gray-700 rounded-xl" />
+								<div class="grid grid-cols-3 gap-4">
+									<div class="h-4 bg-gray-700 rounded-xl col-span-1" />
+									<div class="h-4 bg-gray-700 rounded-xl col-span-2" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</Card>
+				<div class="flex md:flex-row flex-col">
+					<Card className="flex grow mr-6 min-w-max">
+						<div class="animate-pulse">
+							<div class="flex-1 space-y-6 py-1">
+								<div class="h-4 bg-gray-700 rounded-xl" />
+								<div class="space-y-3">
+									<div class="h-4 bg-gray-700 rounded-xl" />
+									<div class="grid grid-cols-3 gap-4">
+										<div class="h-4 bg-gray-700 rounded-xl col-span-1" />
+										<div class="h-4 bg-gray-700 rounded-xl col-span-2" />
+									</div>
+									<div class="h-4 bg-gray-700 rounded-xl" />
+								</div>
+							</div>
+						</div>
+					</Card>
+					<Card className="flex grow min-w-max">
+						<div class="animate-pulse">
+							<div class="flex-1 space-y-6 py-1">
+								<div class="h-4 bg-gray-700 rounded-xl" />
+								<div class="space-y-3">
+									<div class="h-4 bg-gray-700 rounded-xl" />
+									<div class="grid grid-cols-3 gap-4">
+										<div class="h-4 bg-gray-700 rounded-xl col-span-2" />
+										<div class="h-4 bg-gray-700 rounded-xl col-span-1" />
+									</div>
+									<div class="h-4 bg-gray-700 rounded-xl" />
+								</div>
+							</div>
+						</div>
+					</Card>
+				</div>
+			</div>
+		{/if}
 	</main>
 </div>
